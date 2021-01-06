f7c16477cc7aea6b54b838d1bb29a0e4f4db1184
Pat Maguire <prmaguire@email.neit.edu>
2021-01-05 21:27:52 -0500

    First Angular Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myHeroesApp';
+  title = 'Pat\'s Hero App!';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wolverine' },
+  { id: 12, name: 'Venom' },
+  { id: 13, name: 'Cyclops' },
+  { id: 14, name: 'Storm' },
+  { id: 15, name: 'Nightcrawler' },
+  { id: 16, name: 'Ice Man' },
+  { id: 17, name: 'Pyro' },
+  { id: 18, name: 'Colossus' },
+  { id: 19, name: 'Deadpool' },
+  { id: 20, name: 'Phoenix' }
+];
